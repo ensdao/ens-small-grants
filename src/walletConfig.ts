bcef848b58dfa71274a76e9e6c56eeea32ca9422
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-09-12 02:39:48 -0400

    Update walletConfig.ts

--- src/walletConfig.ts
+++ src/walletConfig.ts
@@ -9,7 +9,7 @@ if (!WALLETCONNECT_ID) {
   throw new Error('Missing NEXT_PUBLIC_WALLETCONNECT_ID');
 }
 
-export const chains = [mainnet, goerli];
+export const chains = [mainnet];
 
 const { publicClient, webSocketPublicClient } = configureChains(chains, [publicProvider()]);
 
